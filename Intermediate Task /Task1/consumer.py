cd0471ec64e21eb5a09c2605f5d1b95094a4cd85
InternPro Reference <116167850+InternProO@users.noreply.github.com>
2024-08-13 02:20:59 +0530

    Delete Intermediate Task /Task1/consumer.py

--- Intermediate Task /Task1/consumer.py
+++
@@ -1,9 +0,0 @@
-from kafka import KafkaConsumer
-import json
-
-consumer = KafkaConsumer('iot_data', bootstrap_servers='localhost:9092', value_deserializer=lambda x: json.loads(x.decode('utf-8')))
-
-for message in consumer:
-    data = message.value
-    # Perform real-time analysis here
-    print(f"Received data: {data}")
